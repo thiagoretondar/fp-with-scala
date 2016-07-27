ed90802b99e56e95e44f28aedbf55de83b133860
Thiago Retondar <t.retondar@gmail.com>
2016-07-26 23:42:30 -0300

    some exercises of chapter 3

--- chapter3/example1.scala
+++ chapter3/example1.scala
@@ -19,6 +19,36 @@ object List {
 		if (as.isEmpty) Nil
 		else Cons(as.head, apply(as.tail: _*))
 
+	def tail[A](ts: List[A]) = ts match {
+		case Nil => sys.error("init of empty error")
+		case Cons(x, xs) => xs
+	}
+
+	def drop[A](ts: List[A], n: Int) = {
+		if (n > 0) {
+			ts match {
+				case Nil => sys.error("init of empty list")
+				case Cons(x, xs) => drop(xs, n - 1)
+			}
+		} else ts
+	}
+
+	def dropWhile[A](l: List[A])(predicate: A => Boolean): List[A] = {
+		ts match {
+			case Nil => sys.error("init of empty list")
+			case Cons(head, tail) =>
+				if (!predicate(head))
+					dropWhile(tail)(predicate)
+				else
+					ts
+		}
+	}
+
+	def setHead[A](ts: List[A], head: A): List[A] = ts match {
+		case Nil => Cons(head, List())
+		case Cons(x, xs) => Cons(head, xs)
+	}
+
 	val example = Cons(1, Cons(2, Cons(3, Nil)))
 	val example2 = List(1, 2, 3)
 	val total = sum(example)
